c9c04d10aa4da32c3660ae6583847eabe042099e
Michael Becco <e4e6d4d5@gmail.com>
2016-08-05 10:45:42 -0700

    Test Devise Sanitized Params for username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
